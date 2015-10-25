14700a679cf4019de72df4e39c89e36054b62ba0
Joshua Cox <joshua.n.cox@gmail.com>
2015-10-24 23:34:34 -0700

    Adding pagination to topic page
    - Simple pagination added
    - Nav bar added fixed with scroll to control pages

--- src/components/topic.jsx
+++ src/components/topic.jsx
@@ -3,6 +3,7 @@ var Actions = require('../actions');
 var ImageStore = require('../stores/image-store');
 var Reflux = require('reflux');
 var ImagePreview = require('./image-preview');
+var _ = require('lodash');
 
 module.exports = React.createClass({
 	mixins: [
@@ -10,28 +11,68 @@ module.exports = React.createClass({
 	],
 	getInitialState: function() {
 		return {
-			images: []
+			images: [],
+			page: 0
 		}
 	},
 	componentWillMount: function() {
 		Actions.getImages(this.props.params.id);
+		// Actions.getImages(this.props.params.id + '/viral/' + this.state.page);
+	},
+	componentDidMount: function() {
+		// var scroll = _.debounce(function() {
+		// 	console.log('scrollin');
+		// }, 50);
+		window.addEventListener('scroll', this.handleScroll);
 	},
 	componentWillReceiveProps: function(nextProps) {
 		Actions.getImages(nextProps.params.id);
+		// Actions.getImages(nextProps.params.id + '/viral/' + this.state.page);
+	},
+	componentDidUpdate: function() {
+		console.log('updating');
+		window.scroll(0,0);
 	},
 	render: function() {
 		return <div className='topic'>
+			<div className='nav container-fluid'>
+				<button className='last' onClick={this.onClickLast}>&lt;&lt;</button>
+				<button className='next' onClick={this.onClickNext}>next</button>
+			</div>
 			{this.renderImages()}
 		</div>
 	},
 	renderImages: function() {
-		return this.state.images.slice(0, 20).map(function(image) {
+		return this.state.images.map(function(image) {
 			return <ImagePreview key={image.id} {...image} />
 		});
+	},
+	handleScroll: function() {
+		var nav = document.querySelector('.topic .nav');
+		var top = nav.getBoundingClientRect().top;
+		//console.log(window.scrollY + ' : ' + top);
+
+		if (window.scrollY < 30) {nav.classList.remove('fixed')}
+		else if (top <= 0) {nav.classList.add('fixed')}
 	},
 	onChange: function(event, images) {
 		this.setState({
 			images: images
 		});
+	},
+	onClickNext: function() {
+		this.setState({
+			page: this.state.page += 1
+		});
+		Actions.getImages(this.props.params.id + '/viral/' + this.state.page);
+	},
+	onClickLast: function() {
+		var newPage = (this.state.page == 0) ? 0 : this.state.page - 1;
+
+		this.setState({
+			page: newPage
+		});
+		Actions.getImages(this.props.params.id + '/viral/' + this.state.page);
 	}
 });
+
